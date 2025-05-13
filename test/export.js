e7f3e20b96113b198355964becf5799f366e0d3d
Jakob Voss <voss@gbv.de>
2025-05-13 15:32:06 +0200

    Remove duplicated test

--- test/export.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai"
-import { TSVReader } from "../index.js"
-
-describe("export", () => {
-  it("should export TSVReader", () => {
-    expect(TSVReader).to.be.a("function")
-  })
-})
